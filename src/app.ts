d80a6081ae3c35725c6fe1bd2bd21b3535426b82
fayis <fayfayis74@gmail.com>
2025-03-17 16:14:27 +0530

    cors done , deploy round 1

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,16 @@ import cors from 'cors';
 import authRoutes from './routes/authRoutes.js';
 import userRoutes from './routes/userRoutes.ts';
 import taskRoutes from './routes/taskRoutes.ts';
+import dotenv from "dotenv";
+
+dotenv.config();
+const frontendUrl = process.env.FRONTEND_URL;
 
 const app = express()
-app.use(cors())
+app.use(cors({
+    origin: frontendUrl,
+    credentials: true, 
+}))
 app.use(express.json())
 
 app.use('/auth',authRoutes)
